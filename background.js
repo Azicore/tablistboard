e7a8f179777b9bf43c7af9483cf1f38da429a37f
Azicore <Azicore@users.noreply.github.com>
2017-11-14 00:29:07 +0900

    Added ver.1.1.0.

--- background.js
+++ background.js
@@ -1,61 +1,232 @@
 
-// メインページ
-var mainPage = 'index.html';
+// --------------------------------------------------------------------------------
+// # 基本変数
 
-// メインページのタブのID
-var mainPageTabId = null;
+// メインページとサイドバーのHTML
+var MAIN_PAGE = {
+	ja: 'main_ja.html',
+	en: 'main_en.html'
+};
+var SIDEBAR_PAGE = 'sidebar.html';
 
-// アドオン起動直前に開いていたタブのID
-var lastTabId;
+// タブリスト情報のキー
+var TAB_VALUE_KEY    = 'tabInfo';
+var WINDOW_VALUE_KEY = 'windowInfo';
 
-// タブ切り替え（targetに切り替えて、selfを閉じる）
-var switchTab = function(self, target) {
+// ウィンドウごとのメインページのタブのID
+var mainPageTabIds = {};
+
+// ウィンドウごとのアドオン起動直前に開いていたタブのID
+var lastTabIds = {};
+
+// 言語
+var lang = browser.i18n.getUILanguage();
+lang = /^ja/.test(lang) ? 'ja' : 'en';
+
+
+// --------------------------------------------------------------------------------
+// # バックグラウンド関数
+
+// タブ切り替え
+var switchTab = function(windowId, self, target) {
+	// 依存変数：mainPageTabIds
+	// targetに切り替える
 	if (target != null) browser.tabs.update(target, { active: true });
-	browser.tabs.remove(self);
-	// メインページを閉じた場合は変数をnullにセット
-	if (self == mainPageTabId) mainPageTabId = null;
+	// selfを閉じる
+	if (self != null) {
+		browser.tabs.remove(self);
+		// メインページを閉じた場合は変数をnullにセット
+		if (self == mainPageTabIds[windowId]) mainPageTabIds[windowId] = null;
+	}
 };
 
-// タブ情報取得
-var getAllTabs = function() {
-	return browser.tabs.query({ currentWindow: true });
+// タブとウィンドウに保存した情報を取得
+var getTabListInfo = function(windowId, callback) {
+	// 依存変数：TAB_VALUE_KEY, WINDOW_VALUE_KEY
+	var tabs;
+	Promise.all([
+		browser.tabs.query({ windowId: windowId }).then(function(obj) {
+			tabs = obj;
+			var tabInfo = [];
+			for (var i = 0; tabs.length > i; i++) {
+				tabInfo[i] = browser.sessions.getTabValue(tabs[i].id, TAB_VALUE_KEY);
+			}
+			return Promise.all(tabInfo);
+		}),
+		browser.sessions.getWindowValue(windowId, WINDOW_VALUE_KEY)
+	]).then(function(obj) {
+		callback(tabs, obj[0], (obj[1] || {}).lists);
+	});
 };
 
-// アドオンの起動
+// タブとウィンドウに保存した情報を全て削除
+var removeTabListInfo = function(windowId, callback) {
+	// 依存変数：TAB_VALUE_KEY, WINDOW_VALUE_KEY
+	Promise.all([
+		browser.tabs.query({ windowId: windowId }).then(function(tabs) {
+			var objs = [];
+			for (var i = 0; tabs.length > i; i++) {
+				objs[i] = browser.sessions.removeTabValue(tabs[i].id, TAB_VALUE_KEY);
+			}
+			return Promise.all(objs);
+		}),
+		browser.sessions.removeWindowValue(windowId, WINDOW_VALUE_KEY)
+	]).then(function(obj) {
+		callback();
+	});
+};
+
+// アクティブなタブを取得
+var getActiveTab = function(windowId) {
+	// 依存変数：なし
+	return new Promise(function(resolve, reject) {
+		browser.tabs.query({ active: true, windowId: windowId }).then(function(tabs) {
+			resolve(tabs[0].id);
+		}, reject);
+	});
+};
+
+
+// --------------------------------------------------------------------------------
+// # 固定共通オブジェクト・関数
+
+// 国際化テキスト
+var i18nTexts = {
+	ADD_TAB_BUTTON      : browser.i18n.getMessage('addTabButton'),
+	REMOVE_LIST_BUTTON  : browser.i18n.getMessage('removeListButton'),
+	NEW_LIST_NAME       : browser.i18n.getMessage('newListName'),
+	REMOVE_LIST_CONFIRM : browser.i18n.getMessage('removeListConfirm'),
+	CLOSE_TAB_CONFIRM   : browser.i18n.getMessage('closeTabConfirm'),
+	TITLE               : browser.i18n.getMessage('title'),
+	CONFIG_TITLE        : browser.i18n.getMessage('configTitle'),
+	CONFIG_RESET_CONFIRM: browser.i18n.getMessage('configResetConfirm'),
+	CONFIG_RESET_DONE   : browser.i18n.getMessage('configResetDone'),
+	SIDEBAR_ERROR_MAIN  : browser.i18n.getMessage('sidebarErrorMain'),
+	SIDEBAR_ERROR_NOINFO: browser.i18n.getMessage('sidebarErrorNoinfo')
+};
+
+// 設定の読み込みと保存
+var config = (function() {
+	var obj = {};
+	// storageから変数にコピー（初回のみ）
+	browser.storage.local.get('config').then(function(o) {
+		var config = o.config || {
+			wordWrap   : true,
+			oldTab     : true,
+			oldTabTerm : 168,
+			lastTab    : true,
+			newTab     : true,
+			closeTab   : true,
+			removeList : true,
+			infoVer    : '0.0.0'
+		};
+		for (var i in config) {
+			obj[i] = config[i];
+		}
+	});
+	// 与えられた設定をstorageと変数にコピー
+	obj.update = function(config) {
+		for (var i in config) {
+			this[i] = config[i];
+		}
+		return browser.storage.local.set({config: config});
+	};
+	return obj;
+})();
+
+// 全てのリストの情報を更新して保存する
+var updateListInfo = function($, windowId) {
+	// 依存変数：WINDOW_VALUE_KEY
+	var lists = [];
+	var colId = 0;
+	$('.column_block').each(function() {
+		var $columnBlock = $(this);
+		var $tablistBlock = $columnBlock.find('.tablist_block');
+		// 空のカラムは削除
+		if ($tablistBlock.length == 0) {
+			$columnBlock.remove();
+			return true;
+		}
+		$tablistBlock.each(function(i) {
+			var $this = $(this);
+			lists.push({
+				listId: +$this.attr('data-list-id'),
+				column: colId,
+				order : i + 1,
+				title : $this.find('.tablist_title').text()
+			});
+		});
+		colId++;
+	});
+	browser.sessions.setWindowValue(windowId, WINDOW_VALUE_KEY, { lists: lists });
+};
+
+// 全てのタブの情報を更新して保存する
+var updateTabInfo = function($, windowId) {
+	// 依存変数：TAB_VALUE_KEY
+	$('.tablist_block').each(function() {
+		var $tablistBlock = $(this);
+		var listId = +$tablistBlock.attr('data-list-id');
+		var $tabItems = $tablistBlock.find('.tabitem').each(function(i) {
+			var $this = $(this);
+			var tabId = +$this.attr('data-tab-id');
+			browser.sessions.setTabValue(tabId, TAB_VALUE_KEY, {
+				listId: listId,
+				order: i + 1
+			});
+		});
+		$tablistBlock.find('.remove_list_btn').toggle($tabItems.length == 0);
+	});
+};
+
+
+// --------------------------------------------------------------------------------
+// # 動作定義
+
+// メインページの起動
 var start = function(toggle) {
-	// 起動直前に開いていたタブのIDを取得
-	browser.tabs.query({ active: true, currentWindow: true }).then(function(tab) {
+	// 依存変数：switchTab, getActiveTab, mainPageTabIds, lastTabIds, MAIN_PAGE, lang
+	var windowId;
+	// 現在のウィンドウとタブを取得
+	browser.windows.getCurrent().then(function(window) {
+		windowId = window.id;
+		return getActiveTab(windowId);
+	}).then(function(currentTabId) {
 		// メインページで再び起動しようとした場合
-		if (tab[0].id == mainPageTabId) {
+		if (mainPageTabIds[windowId] != null && currentTabId == mainPageTabIds[windowId]) {
 			// ショートカットの場合は最後のタブに戻り、アドオンボタンの場合は何もしない
-			if (toggle) switchTab(mainPageTabId, lastTabId);
+			if (toggle) switchTab(windowId, mainPageTabIds[windowId], lastTabIds[windowId]);
 		} else {
-			lastTabId = tab[0].id;
+			lastTabIds[windowId] = currentTabId;
 			// メインページのタブを再利用できる場合は再利用
-			(mainPageTabId == null
+			(mainPageTabIds[windowId] == null
 				? Promise.reject()
-				: browser.tabs.update(mainPageTabId, { url: mainPage, active: true })
+				: browser.tabs.update(mainPageTabIds[windowId], { url: MAIN_PAGE[lang], active: true })
 			// 再利用できない場合（通常）は新規タブでメインページを開く
 			).catch(function() {
-				browser.tabs.create({ url: mainPage }).then(function(tab) {
-					mainPageTabId = tab.id;
+				browser.tabs.create({ url: MAIN_PAGE[lang] }).then(function(tab) {
+					mainPageTabIds[windowId] = tab.id;
 				});
 			});
 		}
 	});
 };
 
-// 既知のタブから開いた場合は同じリストIDを保持
+// 新しいタブが開いたとき
 browser.tabs.onCreated.addListener(function(tab) {
-	if (tab.openerTabId != null) {
-		browser.sessions.getTabValue(tab.openerTabId, 'tabInfo').then(function(tabInfo) {
-			if (tabInfo) browser.sessions.setTabValue(tab.id, 'tabInfo', { listId: tabInfo.listId });
-		});
-	}
+	if (!tab.openerTabId) return;
+	browser.sessions.getTabValue(tab.openerTabId, TAB_VALUE_KEY).then(function(tabInfo) {
+		if (tabInfo) {
+			browser.sessions.setTabValue(tab.id, TAB_VALUE_KEY, {
+				listId: tabInfo.listId,
+				order : (tabInfo.order + Math.floor(tabInfo.order + 1)) / 2
+			});
+		}
+	});
 });
 
 // アドオンボタンのクリック
-chrome.browserAction.onClicked.addListener(function() {
+browser.browserAction.onClicked.addListener(function() {
 	start(false);
 });
 
--- main.js
+++ main.js
@@ -1,124 +1,50 @@
 
 // ページロード時に起動
-document.addEventListener('DOMContentLoaded', function() {
+window.jQuery(function($) {
+	'use strict';
 	
-	// 国際化
-	$('.i18n').each(function() {
-		var $this = $(this);
-		$this.html(browser.i18n.getMessage($this.html().replace(/^__MSG_(.+)__$/, '$1')));
-	});
-	var i18nTexts = {
-		ADD_TAB_BUTTON      : browser.i18n.getMessage('addTabButton'),
-		REMOVE_LIST_BUTTON  : browser.i18n.getMessage('removeListButton'),
-		NEW_LIST_NAME       : browser.i18n.getMessage('newListName'),
-		REMOVE_LIST_CONFIRM : browser.i18n.getMessage('removeListConfirm'),
-		CLOSE_TAB_CONFIRM   : browser.i18n.getMessage('closeTabConfirm'),
-		TITLE               : browser.i18n.getMessage('title'),
-		CONFIG_TITLE        : browser.i18n.getMessage('configTitle'),
-		CONFIG_RESET_CONFIRM: browser.i18n.getMessage('configResetConfirm'),
-		CONFIG_RESET_DONE   : browser.i18n.getMessage('configResetDone')
-	};
+	// --------------------------------------------------------------------------------
+	// # 基本変数と初期化
 	
 	// バックグラウンドJSのルートオブジェクトを取得
 	var bg = browser.extension.getBackgroundPage();
 	
-	// アドオン起動直前に開いていたタブのID
-	var lastTabId = bg.lastTabId;
+	// メインページとサイドバーとの動作切り替え
+	var isSidebar = location.href.indexOf(bg.SIDEBAR_PAGE) >= 0;
 	
-	// 現在のウィンドウのID
-	var currentWindowId = browser.windows.WINDOW_ID_CURRENT;
+	// 現在のウィンドウのID（起動時に取得）
+	var currentWindowId;
 	
-	// このタブのID
-	var thisTabId;
+	// 国際化テキスト
+	var i18nTexts = bg.i18nTexts;
 	
-	// 設定の読み込みと保存
-	var Config = (function() {
-		// 設定項目の要素
-		var $config = {
-			wordWrap  : $('#conf_wordwrap'),
-			oldTab    : $('#conf_oldtab'),
-			oldTabTerm: $('#conf_oldtabterm'),
-			lastTab   : $('#conf_lasttab'),
-			newTab    : $('#conf_newtab'),
-			closeTab  : $('#conf_closetab'),
-			removeList: $('#conf_removelist')
-		};
-		var obj = {
-			// 設定を読み込んで設定画面に反映
-			load: function() {
-				var self = this;
-				return browser.storage.local.get().then(function(obj) {
-					var config = obj.config || {
-						wordWrap   : true,
-						oldTab     : true,
-						oldTabTerm : 168,
-						lastTab    : true,
-						newTab     : true,
-						closeTab   : true,
-						removeList : true
-					};
-					for (var i in config) {
-						var val = self[i] = config[i];
-						typeof val == 'boolean' ? $config[i].prop('checked', val) : $config[i].val(val);
-					}
-					return Promise.resolve();
-				});
-			},
-			// 設定画面の設定を保存
-			save: function() {
-				var config = {};
-				for (var i in $config) {
-					this[i] = config[i] = $config[i].attr('type') == 'checkbox' ? $config[i].prop('checked') : $config[i].val();
-				}
-				return browser.storage.local.set({config: config});
-			}
-		};
-		obj.load();
-		return obj;
-	})();
+	// 国際化
+//	$('.i18n').each(function() {
+//		var $this = $(this);
+//		$this.html(browser.i18n.getMessage($this.html().replace(/^__MSG_(.+)__$/, '$1')));
+//	});
+	
+	
+	// --------------------------------------------------------------------------------
+	// # 共通関数
 	
 	// 全てのリストの情報を更新して保存する
 	var updateListInfo = function() {
-		var lists = [];
-		var colId = 0;
-		$('.column_block').each(function() {
-			var $columnBlock = $(this);
-			var $tablistBlock = $columnBlock.find('.tablist_block');
-			// 空のカラムは削除
-			if ($tablistBlock.length == 0) {
-				$columnBlock.remove();
-				return true;
-			}
-			$tablistBlock.each(function(i) {
-				var $this = $(this);
-				lists.push({
-					listId: +$this.attr('data-list-id'),
-					column: colId,
-					order : i + 1,
-					title : $this.find('.tablist_title').text()
-				});
-			});
-			colId++;
-		});
-		browser.sessions.setWindowValue(currentWindowId, 'windowInfo', { lists: lists });
+		return bg.updateListInfo($, currentWindowId);
 	};
 	
 	// 全てのタブの情報を更新して保存する
 	var updateTabInfo = function() {
-		$('.tablist_block').each(function() {
-			var $tablistBlock = $(this);
-			var listId = +$tablistBlock.attr('data-list-id');
-			var $tabItems = $tablistBlock.find('.tabitem').each(function(i) {
-				var $this = $(this);
-				var tabId = +$this.attr('data-tab-id');
-				browser.sessions.setTabValue(tabId, 'tabInfo', { listId: listId, order: i + 1 });
-			});
-			$tablistBlock.find('.remove_list_btn').toggle($tabItems.length == 0);
-		});
+		return bg.updateTabInfo($, currentWindowId);
 	};
 	
 	// カラムのHTMLを生成する
 	var createColumnElement = function(col) {
+		// 依存変数：updateListInfo
+		// col = {
+		//     colId: <Number>, // カラムID
+		//     lists: <Array>   // listの配列
+		// }
 		var $column = $('<ul>').addClass('column').sortable({
 			connectWith: '.column',
 			placeholder: 'tablist_block placeholder',
@@ -137,6 +63,12 @@ document.addEventListener('DOMContentLoaded', function() {
 	
 	// リストのHTMLを生成する
 	var createListElement = function(list) {
+		// 依存変数：updateTabInfo, i18nTexts
+		// list = {
+		//     listId: <Number>, // リストID
+		//     title : <String>, // リスト名
+		//     tabs  : <Array>   // tabの配列
+		// }
 		var $tablist = $('<ul>').addClass('tablist').sortable({
 			connectWith: '.tablist',
 			placeholder: 'tabitem placeholder',
@@ -161,79 +93,207 @@ document.addEventListener('DOMContentLoaded', function() {
 	
 	// タブのHTMLを生成する
 	var createTabElement = function(tab) {
+		// 依存変数：bg.config
+		// tab = {
+		//     id        : <Number>,  // タブID
+		//     title     : <String>,  // タブのタイトル
+		//     favIconUrl: <String>,  // faviconのURL
+		//     isOldTab  : <Boolean>, // 一定期間アクセスしていないタブかどうか
+		//     isNewTab  : <Boolean>, // 最近新しく開いたタブかどうか
+		//     isLastTab : <Boolean>, // 直前に開いていたタブかどうか
+		//     isCurrTab : <Boolean>  // 現在開いているタブかどうか
+		// }
+		var config = bg.config;
 		var cls = ['tabitem'];
-		if (Config.oldTab && tab.isOld) cls.push('old_tab');
-		if (Config.newTab && tab.isNewTab) cls.push('new_tab');
-		if (Config.lastTab && tab.id == lastTabId) cls.push('last_tab');
-		if (!Config.wordWrap) cls.push('single_line');
+		if (config.oldTab && tab.isOldTab) cls.push('old_tab');
+		if (config.newTab && tab.isNewTab) cls.push('new_tab');
+		if (config.lastTab && tab.isLastTab) cls.push('last_tab');
+		if (tab.isCurrTab) cls.push('current_tab');
+		if (!config.wordWrap) cls.push('single_line');
 		return $('<li>').addClass(cls.join(' ')).attr('data-tab-id', tab.id)
 			.append($('<div>').addClass('close_tab_btn'))
 			.append($('<img>').addClass('favicon').attr('src', tab.favIconUrl))
 			.append($('<span>').addClass('tabitem_text').html(tab.title));
 	};
 	
-	var tabs, cols = [];
-	Promise.all([
-		bg.getAllTabs(),
-		browser.tabs.getCurrent(),
-		browser.sessions.getWindowValue(currentWindowId, 'windowInfo')
-	]).then(function(obj) {
-		// 全てのタブの情報
-		tabs = obj[0];
-		// このタブの情報
-		thisTabId = obj[1].id;
-		// ウィンドウに保存した情報
-		var lists = (obj[2] || {}).lists;
-		// ウインドウに情報がない場合は新規リストを1つ作成
-		if (!lists || lists.length == 0) lists = [{ listId: Date.now(), column: 0, order: 1, title: i18nTexts.NEW_LIST_NAME }];
-		for (var i = 0; lists.length > i; i++) {
-			var colId = lists[i].column;
-			if (!cols[colId]) cols[colId] = { colId: colId, lists: [] };
-			cols[colId].lists.push(lists[i]);
-		}
-		// カラムごとにソート
-		for (var i = 0; cols.length > i; i++) {
-			cols[i].lists.sort(function(a, b) { return a.order > b.order ? 1 : -1; });
-		}
-		// タブに保存した情報を取得
-		var tabInfo = [];
-		for (var i = 0; tabs.length > i; i++) {
-			if (tabs[i].id == thisTabId) continue;
-			tabInfo[i] = browser.sessions.getTabValue(tabs[i].id, 'tabInfo');
-		}
-		return Promise.all(tabInfo);
-	}).then(function(tabInfo) {
-		// タブ情報を一つのオブジェクトに集約してリストごとに分類する
-		var tabLists = {};
-		var defaultListId = cols[0].lists[0].listId;
-		var defaultOrder = 1e10;
-		var limitTime = Date.now() - Config.oldTabTerm * 36e5;
-		for (var i = 0; tabs.length > i; i++) {
-			if (tabs[i].id == thisTabId) continue;
-			var listId = (tabInfo[i] || {}).listId || defaultListId; // 新規タブ
-			var order  = (tabInfo[i] || {}).order || ++defaultOrder;
-			tabs[i].listId = listId;
-			tabs[i].order  = order;
-			if (limitTime > tabs[i].lastAccessed) tabs[i].isOld = true;
-			if (order == defaultOrder) tabs[i].isNewTab = true;
-			if (!tabLists[listId]) tabLists[listId] = [];
-			tabLists[listId].push(tabs[i]);
-		}
-		// リストごとにソート
-		for (var i in tabLists) {
-			tabLists[i].sort(function(a, b) { return a.order > b.order ? 1 : -1; });
-		}
-		// カラム情報とマージしてHTML生成
-		for (var i = 0; cols.length > i; i++) {
-			var lists = cols[i].lists;
-			for (var j = 0; lists.length > j; j++) {
-				var listId = lists[j].listId;
-				lists[j].tabs = tabLists[listId] || [];
+	
+	// --------------------------------------------------------------------------------
+	// # メイン処理
+	
+	// メインページのタブ一覧生成
+	var generateMainBoard = function() {
+		bg.getTabListInfo(currentWindowId, function(tabs, tabInfo, lists) {
+			var lastTabId     = bg.lastTabIds[currentWindowId];
+			var mainPageTabId = bg.mainPageTabIds[currentWindowId];
+			var orderFunc = function(a, b) { return a.order > b.order ? 1 : -1; };
+			// ウィンドウに情報がない場合は新規リストを1つ作成
+			if (!lists || lists.length == 0) {
+				lists = [{ listId: Date.now(), column: 0, order: 1, title: i18nTexts.NEW_LIST_NAME }];
 			}
-			$('#main').append(createColumnElement(cols[i]));
+			// カラムごとにリストを分類
+			var cols = [];
+			lists.isExistent = {};
+			for (var i = 0; lists.length > i; i++) {
+				var colId = lists[i].column;
+				if (!cols[colId]) cols[colId] = { colId: colId, lists: [] };
+				cols[colId].lists.push(lists[i]);
+				lists.isExistent[lists[i].listId] = true;
+			}
+			// カラムごとにリストをソート
+			for (var i = 0; cols.length > i; i++) {
+				cols[i].lists.sort(orderFunc);
+			}
+			// リストごとにタブを分類
+			var tabLists = {};
+			var defaultListId = cols[0].lists[0].listId;
+			var defaultOrder = 1e10;
+			var limitTime = Date.now() - bg.config.oldTabTerm * 36e5;
+			for (var i = 0; tabs.length > i; i++) {
+				if (tabs[i].id == mainPageTabId) continue;
+				var listId = (tabInfo[i] || {}).listId || defaultListId;
+				var order  = (tabInfo[i] || {}).order  || defaultOrder;
+				if (!lists.isExistent[listId]) listId = defaultListId;
+				tabs[i].listId    = listId;
+				tabs[i].order     = order;
+				tabs[i].isOldTab  = limitTime > tabs[i].lastAccessed;
+				tabs[i].isNewTab  = order == defaultOrder;
+				tabs[i].isLastTab = tabs[i].id == lastTabId;
+				if (!tabLists[listId]) tabLists[listId] = [];
+				tabLists[listId].push(tabs[i]);
+			}
+			// リストごとにタブをソート
+			for (var i in tabLists) {
+				tabLists[i].sort(orderFunc);
+			}
+			// HTML生成
+			for (var i = 0; cols.length > i; i++) {
+				var lists = cols[i].lists;
+				for (var j = 0; lists.length > j; j++) {
+					var listId = lists[j].listId;
+					lists[j].tabs = tabLists[listId] || [];
+				}
+				$('#main').append(createColumnElement(cols[i]));
+			}
+			updateTabInfo();
+			updateListInfo();
+		});
+	};
+	
+	// サイドバーでのタブ一覧生成
+	var generateSidebar = function(currentTabId, func) {
+		bg.getTabListInfo(currentWindowId, function(tabs, tabInfo, lists) {
+			var mainPageTabId = bg.mainPageTabIds[currentWindowId];
+			var orderFunc = function(a, b) { return a.order > b.order ? 1 : -1; };
+			// 画面表示
+			var display = function(obj) {
+				var isError = typeof obj == 'string';
+				$('#sidebar').empty().toggle(!isError).append(isError ? '' : obj);
+				$('#sidebar_msg').toggle(isError).html(isError ? obj : '');
+				if (typeof func == 'function') func();
+			};
+			// メインページを表示中の場合
+			if (currentTabId != null && currentTabId == mainPageTabId) {
+				return display(i18nTexts.SIDEBAR_ERROR_MAIN);
+			}
+			// ウィンドウに情報がない場合
+			if (!lists || lists.length == 0 || currentTabId == null) {
+				return display(i18nTexts.SIDEBAR_ERROR_NOINFO);
+			}
+			// 対象のリストを特定
+			var targetListId;
+			for (var i = 0; tabs.length > i; i++) {
+				if (tabs[i].id == currentTabId) {
+					targetListId = (tabInfo[i] || {}).listId;
+					break;
+				}
+			}
+			if (targetListId == null) {
+				return display(i18nTexts.SIDEBAR_ERROR_NOINFO);
+			}
+			// 対象リストのタブを集約
+			var tabList = [];
+			var defaultOrder = 1e10;
+			var limitTime = Date.now() - bg.config.oldTabTerm * 36e5;
+			for (var i = 0; tabs.length > i; i++) {
+				if (tabs[i].id == mainPageTabId) continue;
+				if ((tabInfo[i] || {}).listId != targetListId) continue;
+				var order = (tabInfo[i] || {}).order || defaultOrder;
+				tabs[i].listId    = targetListId;
+				tabs[i].order     = order;
+				tabs[i].isOldTab  = limitTime > tabs[i].lastAccessed;
+				tabs[i].isNewTab  = order == defaultOrder || order % 1 > 0;
+				tabs[i].isCurrTab = tabs[i].id == currentTabId;
+				tabList.push(tabs[i]);
+			}
+			// タブをソート
+			tabList.sort(orderFunc);
+			// HTML生成
+			for (var i = 0; lists.length > i; i++) {
+				if (lists[i].listId == targetListId) {
+					lists[i].tabs = tabList;
+					return display(createListElement(lists[i]));
+				}
+			}
+		});
+	};
+	
+	// ウィンドウIDを取得して起動
+	browser.windows.getCurrent().then(function(window) {
+		currentWindowId = window.id;
+		
+		// メインページの場合
+		if (!isSidebar) {
+			generateMainBoard();
+			return;
 		}
-		updateTabInfo();
-		updateListInfo();
+		
+		// サイドバーの場合
+		var getActiveTab = function() {
+			return bg.getActiveTab(currentWindowId);
+		};
+		getActiveTab().then(function(tabId) {
+			generateSidebar(tabId);
+		});
+		// タブが切り替わったとき
+		browser.tabs.onActivated.addListener(function(obj) {
+			// ※obj.tabIdはまれに不正確なため使用不可。
+			if (obj.windowId != currentWindowId) return;
+			getActiveTab().then(function(tabId) {
+				generateSidebar(tabId, updateTabInfo);
+			});
+		});
+		// タブが更新されたとき
+		browser.tabs.onUpdated.addListener(function(tabId, obj, tab) {
+			if (!obj.url && !obj.title || tab.windowId != currentWindowId) return;
+			getActiveTab().then(function(tabId) {
+				setTimeout(function() { generateSidebar(tabId); }, 200);
+			});
+		});
+		// タブが閉じられたとき
+		browser.tabs.onRemoved.addListener(function(tabId, obj) {
+			if (obj.windowId != currentWindowId) return;
+			getActiveTab().then(function(tabId) {
+				setTimeout(function() { generateSidebar(tabId); }, 200);
+			});
+		});
+	
+	});
+	
+	
+	// --------------------------------------------------------------------------------
+	// # 各種イベント
+	
+	// 初期化ボタン
+	$('#conf_reset').on('click', function(e) {
+		if (e.shiftKey && e.ctrlKey) {
+			browser.storage.local.remove(['config', 'update']); // ## For debug
+			return;
+		}
+		if (!confirm(i18nTexts.CONFIG_RESET_CONFIRM)) return;
+		bg.removeTabListInfo(currentWindowId, function() {
+			alert(i18nTexts.CONFIG_RESET_DONE);
+			bg.switchTab(currentWindowId, bg.mainPageTabIds[currentWindowId]);
+		});
 	});
 	
 	// リスト追加ボタン
@@ -248,82 +308,134 @@ document.addEventListener('DOMContentLoaded', function() {
 		);
 		updateListInfo();
 	});
-	// 初期化ボタン
-	$('#conf_reset').on('click', function() {
-		if (!confirm(i18nTexts.CONFIG_RESET_CONFIRM)) return;
-		browser.sessions.removeWindowValue(currentWindowId, 'windowInfo');
-		for (var i = 0; tabs.length > i; i++) {
-			browser.sessions.removeTabValue(tabs[i].id, 'tabInfo');
-		}
-		alert(i18nTexts.CONFIG_RESET_DONE);
-		bg.switchTab(thisTabId);
-	});
-	$(document)
+	
 	// リスト削除ボタン
-	.on('click', '.remove_list_text', function() {
+	$(document).on('click', '.remove_list_text', function() {
 		var $tablistBlock = $(this).parent().parent();
 		var listTitle = $tablistBlock.find('.tablist_title').text();
-		if (!Config.removeList || confirm(i18nTexts.REMOVE_LIST_CONFIRM.replace('%s', listTitle))) {
+		if (!bg.config.removeList || confirm(i18nTexts.REMOVE_LIST_CONFIRM.replace('%s', listTitle))) {
 			$tablistBlock.remove();
 			updateListInfo();
 		}
-	})
+	});
+	
 	// タブ追加ボタン
-	.on('click', '.add_tab_text', function() {
+	$(document).on('click', '.add_tab_text', function() {
 		var $tablist = $(this).parent().parent().find('.tablist');
 		browser.tabs.create({ active: false }).then(function(tab) {
 			$tablist.append(createTabElement(tab));
 			updateTabInfo();
 		});
-	})
+	});
+	
 	// タブ削除ボタン
-	.on('click', '.close_tab_btn', function(e) {
-		if (!Config.closeTab || confirm(i18nTexts.CLOSE_TAB_CONFIRM)) {
+	$(document).on('click', '.close_tab_btn', function(e) {
+		if (!bg.config.closeTab || confirm(i18nTexts.CLOSE_TAB_CONFIRM)) {
 			var $tabitem = $(this).parent();
-			bg.switchTab(+$tabitem.attr('data-tab-id'));
+			bg.switchTab(currentWindowId, +$tabitem.attr('data-tab-id'));
 			$tabitem.remove();
 			updateTabInfo();
 		}
 		e.stopPropagation();
-	})
+	});
+	
 	// タブ選択
-	.on('click', '.tabitem', function() {
-		bg.switchTab(thisTabId, +$(this).attr('data-tab-id'));
-	})
+	$(document).on('click', '.tabitem', function() {
+		bg.switchTab(currentWindowId, bg.mainPageTabIds[currentWindowId], +$(this).attr('data-tab-id'));
+	});
+	
 	// リスト名変更
-	.on('click', '.tablist_title_text', function() {
+	$(document).on('click', '.tablist_title_text', function() {
+		if (isSidebar) return;
 		var $title = $(this).parent();
 		var title = $title.text();
 		var $input = $('<input type="text">').val(title).on('blur', function() {
 			var $this = $(this);
-			$title.empty().append($('<span>').addClass('tablist_title_text').text($this.val()));
+			var newTitle = $this.val();
+			$title.empty().append($('<span>').addClass('tablist_title_text').text(newTitle !== '' ? newTitle : title));
 			$this.remove();
 			updateListInfo();
 		});
 		$title.empty().append($input);
 		$input.focus();
 	});
-	// 設定画面の切り替え
-	var toggleConf = function(toggle) {
-		$('#main').toggle(!toggle);
-		$('#config').toggle(!!toggle);
-		$('.header_btn_main').toggle(!toggle);
-		$('.header_btn_conf').toggle(!!toggle);
-		$('h1').html(toggle ? i18nTexts.CONFIG_TITLE : i18nTexts.TITLE);
-	};
-	$('#conf_open').on('click', function() {
-		toggleConf(true);
-	});
-	$('#conf_cancel').on('click', function() {
-		Config.load().then(function() {
+	
+	
+	// --------------------------------------------------------------------------------
+	// # 設定画面
+	
+	// 設定の読み込みと保存
+	(function() {
+		// 設定項目の要素
+		var $config = {
+			wordWrap  : $('#conf_wordwrap'),
+			oldTab    : $('#conf_oldtab'),
+			oldTabTerm: $('#conf_oldtabterm'),
+			lastTab   : $('#conf_lasttab'),
+			newTab    : $('#conf_newtab'),
+			closeTab  : $('#conf_closetab'),
+			removeList: $('#conf_removelist')
+		};
+		// 設定を読み込んで設定画面に反映
+		var loadConf = function() {
+			for (var i in bg.config) {
+				if (!$config[i]) continue;
+				var val = bg.config[i];
+				typeof val == 'boolean' ? $config[i].prop('checked', val) : $config[i].val(val);
+			}
+		};
+		// 設定画面の切り替え
+		var toggleConf = function(toggle) {
+			$('#main, .header_btn_main').toggle(!toggle);
+			$('#config, .header_btn_conf').toggle(!!toggle);
+			$('h1').html(toggle ? i18nTexts.CONFIG_TITLE : i18nTexts.TITLE);
+		};
+		// 設定ボタン
+		$('#conf_open').on('click', function() {
+			toggleConf(true);
+		});
+		// キャンセルボタン
+		$('#conf_cancel').on('click', function() {
+			loadConf();
 			toggleConf(false);
 		});
-	});
-	$('#conf_ok').on('click', function() {
-		Config.save().then(function() {
-			toggleConf(false);
-			location.reload();
+		// 保存ボタン
+		$('#conf_ok').on('click', function() {
+			var config = {};
+			for (var i in $config) {
+				config[i] = $config[i].attr('type') == 'checkbox' ? $config[i].prop('checked') : $config[i].val();
+			}
+			bg.config.update(config).then(function() {
+				toggleConf(false);
+				location.reload();
+			});
+		});
+		// 初期化
+		loadConf();
+	})();
+	
+	
+	// --------------------------------------------------------------------------------
+	// # その他
+	
+	// 更新情報を表示
+	browser.storage.local.get('update').then(function(o) {
+		var infoVer = (o.update || { infoVer: '0.0.0' }).infoVer;
+		var $info = $('#info');
+		var $infoClose = $('#info_close');
+		var newInfoVer = $info.attr('data-info-ver');
+		$info.toggle(newInfoVer > infoVer).on({
+			mouseenter: function() { $infoClose.show(); },
+			mouseleave: function() { $infoClose.hide(); }
+		});
+		$infoClose.on('click', function() {
+			browser.storage.local.set({
+				update: { infoVer: newInfoVer }
+			});
+			$info.addClass('info_closing');
+			setTimeout(function() { $info.hide(); }, 600);
 		});
 	});
+
 
 });
